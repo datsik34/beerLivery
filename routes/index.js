c7a18a3e174b69fc98e619cce1b747a12e3c4fcb
Benjamin Jullian <ben.jullian@gmail.com>
2018-04-23 14:09:52 +0200

    index data cards

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,24 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  const cardsData = [
+    {
+      title: "Fraicheur",
+      data: "Une bière fraiche, c'est pas une meilleure bière ? C'est pourquoi lors de la livraison, votre bière sera toujours à bonne température, pour votre plus grand bonheur."
+    }, {
+      title: "Qualité",
+      data: "Nous avons sélectionné pour vous le meilleur de la bière du marché."
+    }, {
+      title: "Rapidité",
+      data: "Quoi de mieux qu'un client content ? Nous estimons que vous ne devez pas trop attendre, pour cette raison, nous nous engageons à vous livrer dans les plus brefs délais."
+    }
+  ];
+  res.render('index', {cardsData});
 });
 
-router.get('/catalogue', function(req, res, next){
+router.get('/catalogue', function(req, res, next) {
   res.render('catalogue');
 });
 
